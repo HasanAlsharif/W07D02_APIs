c09c541eaad97cc5151c353a7c7882fd494e30cc
Hasan Alsharif <hasanxalsharif@gmail.com>
2021-05-11 19:13:14 +0300

    Q5

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Q1
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -5,11 +6,14 @@ const port = 3000;
 // a middleware that enables us to read the received JSON data
 app.use(express.json());
 
+// Q2
 const todos = [
-    { todo: " wake up", isCompleted: false },
+    { todo: "wake up", isCompleted: false },
     { todo: "Eat Breakfast", isCompleted: false }
 ];
 
+
+// Q3
 //http://localhost:3000/todos
 app.get("/todos", (req, res) => {
     // set the response status code to 200 (OK)
@@ -19,7 +23,7 @@ app.get("/todos", (req, res) => {
   });
 
   
-
+// Q4
   //http://localhost:3000/create/todo
 app.post("/create/todo", (req, res) => {
    
@@ -30,17 +34,38 @@ app.post("/create/todo", (req, res) => {
 
   });
 
-
-
-
-
   
+// Q5
+  //http://localhost:3000/update/todo/:name
+    app.put("/update/todo/:name", (req, res) => {
+    const update = req.params.name
+    let i
+    const found = todos.find((element, index) => {
+        i=index
+        return element.todo === update;
+      });
+    
+      if (found) {
+        res.status(201);
+        const updatedobject = {todo: req.body.todo, isCompleted: req.body.isCompleted};    
+        todos[i] = updatedobject
+        res.json(todos);
+
+        } else {
+        // set the response status code to 404 (Not Found)
+        res.status(404);
+        res.json("todo task not found");
+      }
+  });
 
 
 
 
 
 
+// Q6
+// Q7
+// Q8
 
 
 
